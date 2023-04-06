7429b594a161114def6930832c019cbb2553a1a4
Lennie <lennie.mbotela@student.moringaschool.com>
2023-04-06 16:35:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function saturdayFun(doThis="roller-skate") {
+    return `This Saturday, I want to ${doThis}!`
+    }
+    saturdayFun()
+    
+    const mondayWork = function(goHere="go to the office"){
+     return `This Monday, I will ${goHere}.` 
+    }
+    mondayWork()
+
+    function wrapAdjective (one="%") {
+        return function(stringOne="you are a human"){
+          return `You are ${one}${stringOne}${one}!`
+        }
+    }
